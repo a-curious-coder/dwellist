7f0ed5bc146dc562b97c3ef8816031baa28bf731
a-curious-coder <callummclennan1@googlemail.com>
2023-10-31 23:10:18 +0000

    Created basic Leaflet JS map

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import csv
 import json
 
-from flask import Flask, render_template
+import pandas as pd
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -10,17 +12,19 @@ def index():
     return render_template("index.html")
 
 
+def read_csv():
+    """Load data as a list of dictionaries"""
+    with open("test.csv", "r", encoding="utf-8") as csv_file:
+        data = list(csv.DictReader(csv_file))
+    return data
+
+
 # Route to fetch marker data
 @app.route("/get_markers")
 def get_markers():
-    # Code to scrape and preprocess data (replace this with your actual logic)
-    markers_data = [
-        {"lat": 51.505, "lng": -0.09, "category": "restaurant"},
-        {"lat": 51.515, "lng": -0.1, "category": "hotel"},
-        {"lat": 51.525, "lng": -0.11, "category": "park"}
-        # Add more marker data as needed
-    ]
-    return json.dumps(markers_data)
+    """Return the marker data as a JSON object"""
+    data = read_csv()
+    return jsonify(data)
 
 
 if __name__ == "__main__":
--- static/script.js
+++ static/script.js
@@ -1,9 +1,10 @@
 // Initialize the map
-var map = L.map('map').setView([51.505, -0.09], 12);
+var map = L.map('map').setView([51.505, -0.09], 13);
 
 // Add OpenStreetMap tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-attribution: '© OpenStreetMap contributors'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 // Initialize an empty layer group for markers
@@ -11,33 +12,46 @@ var markersLayer = L.layerGroup().addTo(map);
 
 // Function to add markers to the map
 function addMarkers(data) {
-markersLayer.clearLayers(); // Clear existing markers
-
-data.forEach(function(marker) {
-    var newMarker = L.marker([marker.lat, marker.lng]);
-    newMarker.addTo(markersLayer);
-});
+    var circle = L.circle([51.508, -0.11], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+    }).addTo(map);
+    // Clear the markers on the map
+    markersLayer.clearLayers();
+    // Creates a popup for the circle
+    circle.bindPopup("I am a circle.");
+    // Ensure each marker is plotted on the map
+    data.forEach(function(marker) {
+        var newMarker = L.marker([marker.Latitude, marker.Longitude]);
+        newMarker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+        newMarker.addTo(markersLayer);
+    });
 }
 
-  // Initial markers on the map
-addMarkers(markersData);
+// Initial markers on the map
+// addMarkers(markersData);
 
 // Filtering logic
 document.getElementById('filterButton').addEventListener('click', function() {
-var category = document.getElementById('categoryFilter').value;
-var filteredMarkers = markersData.filter(function(marker) {
-    return marker.category === category;
-});
-addMarkers(filteredMarkers);
+    var category = document.getElementById('categoryFilter').value;
+    var filteredMarkers = markersData.filter(function(marker) {
+        // return marker.category === category;
+        // Return mark if room_0_price is less than 1000
+        return marker.room_0_price < 1000;
+    });
+    addMarkers(filteredMarkers);
 });
 
 
-  // Fetch data from Flask backend
+// Fetch data from Flask backend
 fetch('/get_markers')
     .then(response => response.json())
     .then(data => {
-    // Add markers to the map based on data received from the server
+        // Add markers to the map based on data received from the server
+        addMarkers(data);
     })
     .catch(error => {
-    console.error('Error:', error);
+        console.error('Error:', error);
     });
