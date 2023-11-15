1b31f577ca04e7f5841f1380c6fd2275efb9553f
a-curious-coder <callummclennan1@googlemail.com>
2023-11-15 22:41:20 +0000

    Add .git and build files to .dockerignore
    This commit adds .git and build files to the .dockerignore file to
    exclude them from Docker build. This ensures that unnecessary files are
    not included in the Docker image.
    
    Set port dynamically in app.py
    
    This commit modifies the app.py file to set the port dynamically. It
    retrieves the port value from the environment variable PORT and if it is
    not available, it uses the default port 5000. This enables flexibility
    in port configuration when running the application.
    
    Fix volume mapping in docker-compose.yml
    
    This commit fixes the volume mapping in the docker-compose.yml file. It
    changes the volume mapping from ./:/app to .:/app, ensuring that the
    current directory is correctly mapped to the app directory in the Docker
    container.
    
    Update Dockerfile
    
    This commit updates the Dockerfile. It installs the latest version of
    pip and uses the --no-cache-dir flag to avoid caching pip packages.
    Additionally, it prints the current directory inside the Docker
    container for debugging purposes.
    
    Update requirements.txt
    
    This commit updates the requirements.txt file. The changes in the
    requirements.txt file are binary files and cannot be shown in the diff.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
 from app import create_app
 
 app = create_app()
+# Get environment variable port or use 5000
+port = int(os.environ.get("PORT", 5000))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
--- scraper/preprocessor.py
+++ scraper/preprocessor.py
@@ -1,7 +1,6 @@
-
 class DataProcessor:
     def __init__(self, config):
         self.config = config
 
     def process(self, data):
-        
+        pass
