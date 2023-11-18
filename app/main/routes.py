9e5b6c66ae768f12b8c687663d2c6474be8dce2f
a-curious-coder <callummclennan1@googlemail.com>
2023-11-18 19:49:04 +0000

    Deleted redundant code

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,27 +2,10 @@ import os
 import csv
 from flask import render_template, jsonify
 from . import main
-import sys
-
-
-def find_index_html(start_path="/dwellist/app"):
-    for root, dirs, files in os.walk(start_path):
-        if "index.html" in files:
-            result = str(os.path.join(root, "index.html"))
-            return result
 
 
 @main.route("/")
 def index():
-    index_path = find_index_html()
-    print(f"Found index.html in: {find_index_html()}", file=sys.stderr)
-
-    # Does index.html exist?
-    exist = os.path.exists(index_path)
-    if not exist:
-        print(f"{index_path} DOES NOT EXIST", file=sys.stderr)
-    else:
-        print(f"{index_path} DOES EXIST\n\n", file=sys.stderr)
     return render_template("index.html")
 
 
